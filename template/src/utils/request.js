6a4b0e772be7b2b1a43ae6c66c49b247d17dc541
lrl <742798240@qq.com>
2021-09-09 20:27:17 +0800

    添加功能：请求超时重新发起请求

--- template/src/utils/request.js
+++ template/src/utils/request.js
@@ -1,43 +1,76 @@
 import axios from 'axios'
 import store from '@/store'
-import { Message } from "element-ui";
-const service = axios.create({
-  baseURL: 'http://47.103.32.42:8081/',
-  headers: {
-    'Content-Type': 'application/json;charset=utf-8'
-  }
-})
+import { Message } from 'element-ui'
+
 // 配置允许跨域携带cookie
 axios.defaults.withCredentials = true
 // 配置超时时间
 axios.defaults.timeout = 10000
+
+const service = axios.create({
+	baseURL: '/',
+	headers: {
+		'Content-Type': 'application/json;charset=utf-8',
+	},
+	retry: 4, //重新发起请求次数
+	retryDelay: 1000,  //再次发起请求间隔
+  timeout: 10000 //超时时间
+})
+
 // 配置请求拦截
 service.interceptors.request.use(
-  config => {
-    config.loading && store.dispatch('setLoading', true)
-    return config;
-  },
-  error => {
-    Message(error)
-    return Promise.error(error);
-  })
+	(config) => {
+		config.loading && store.dispatch('setLoading', true)
+		return config
+	},
+	(error) => {
+		Message(error)
+		return Promise.error(error)
+	}
+)
 
 // axios响应拦截器
 service.interceptors.response.use(
-  response => {
-    response.config.loading && store.dispatch('setLoading', false)
-    // 在这里你可以判断后台返回数据携带的请求码
-    if (response.data.code === 0 || response.data.code === '0') {
-      return response.data.data || response.data
-    } else {
-      // 非200请求报错
-      throw Error(response.msg || response.data.msg || '服务异常')
-    }
-  },
-  error => {
-    error.config.loading && store.dispatch('setLoading', false)
-    // 对响应错误处理
-    return Promise.reject(error)
-  }
+	(response) => {
+		response.config.loading && store.dispatch('setLoading', false)
+
+		// 在这里你可以判断后台返回数据携带的请求码
+		if (response.data.code === 0 || response.data.code === '0') {
+			return response.data.data || response.data
+		} else {
+			// 非200请求报错
+			throw Error(response.msg || response.data.msg || '服务异常')
+		}
+	},
+	(error) => {
+		error.config.loading && store.dispatch('setLoading', false)
+		let config = error.config
+
+    //如果config不存在或retry没设置，返回错误
+		if (!config || !config.retry) return Promise.reject(error)
+
+		// 记录请求次数
+		config.__retryCount = config.__retryCount || 0
+
+		// 大于设置请求次数，返回错误
+		if (config.__retryCount >= config.retry) {
+			return Promise.reject(error)
+		}
+
+		// 请求次数加1
+		config.__retryCount += 1
+
+		// 创建延时发起请求函数
+		var backoff = new Promise(function(resolve) {
+			setTimeout(function() {
+				resolve()
+			}, config.retryDelay || 1)
+		})
+
+		// 延时后再次发起请求
+		return backoff.then(function() {
+			return service(config)
+		})
+	}
 )
 export default service
--- template/vue.config.js
+++ template/vue.config.js
@@ -85,8 +85,8 @@ module.exports = {
 		proxy: {
 			'/': {
 				//这里最好有一个 /
-				// target: 'http://47.103.32.42:8081/',         // 服务器端接口地址
-				target: 'http://localhost:3000/', // 服务器端接口地址
+				target: 'http://47.103.32.42:8081/',         // 服务器端接口地址
+				// target: 'http://localhost:3000/', // 服务器端接口地址
 				ws: true, //如果要代理 websockets，配置这个参数
 				// 如果是https接口，需要配置这个参数
 				changeOrigin: true, //是否跨域
