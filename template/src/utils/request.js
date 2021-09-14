467d1a15fdd0c7e7369444617f35dd8b5167f3a4
lrl <742798240@qq.com>
2021-09-14 16:55:32 +0800

    新增功能/依赖:重复请求取消上一次请求/qs

--- template/src/utils/request.js
+++ template/src/utils/request.js
@@ -1,28 +1,84 @@
 import axios from 'axios'
 import store from '@/store'
+import qs from 'qs';
 import { Message } from 'element-ui'
+
+const CancelToken = axios.CancelToken
 
 const service = axios.create({
 	headers: {
 		'Content-Type': 'application/json;charset=utf-8',
 	},
 	retry: 4, //重新发起请求次数
-	retryDelay: 1000,  //再次发起请求间隔
-  timeout: 10000 //超时时间
+	retryDelay: 1000, //再次发起请求间隔
+	timeout: 10000, //超时时间
 })
 
 //可根据开发环境去设置基础地址
 if (process.env.NODE_ENV === 'development') {
-  service.defaults.baseURL = '/'
+	service.defaults.baseURL = '/'
 } else if (process.env.NODE_ENV === 'test') {
-  service.defaults.baseURL = '/xxx/'
+	service.defaults.baseURL = '/xxx/'
 } else {
-  service.defaults.baseURL = 'http://47.103.32.42:8081/'
+	service.defaults.baseURL = 'http://47.103.32.42:8081/'
+}
+
+let sources = [] // 定义数组用于存储每个ajax请求的取消函数及对应标识
+
+// 定义取消操作
+let removeSource = (config) => {
+	for (let source in sources) {
+		// 当多次请求相同时，取消之前的请求
+		if (
+			(config.method === 'get' &&
+				sources[source].umet ===
+					config.url +
+						'&' +
+						config.method +
+						'&' +
+						qs.stringify(config.params)) ||
+			(config.method === 'post' &&
+				sources[source].umet ===
+					config.url +
+						'&' +
+						config.method +
+						'&' +
+						qs.stringify(config.data))
+		) {
+			sources[source].cancel('取消请求')
+			sources.splice(source, 1)
+		}
+	}
 }
 
 // 配置请求拦截
 service.interceptors.request.use(
 	(config) => {
+		removeSource(config)
+		config.cancelToken = new CancelToken((c) => {
+			// 将取消函数存起来
+			if (config.method === 'get') {
+				sources.push({
+					umet:
+						config.url +
+						'&' +
+						config.method +
+						'&' +
+						qs.stringify(config.params),
+					cancel: c,
+				})
+			} else {
+				sources.push({
+					umet:
+						config.url +
+						'&' +
+						config.method +
+						'&' +
+						qs.stringify(config.data),
+					cancel: c,
+				})
+			}
+		})
 		config.loading && store.dispatch('setLoading', true)
 		return config
 	},
@@ -35,6 +91,8 @@ service.interceptors.request.use(
 // axios响应拦截器
 service.interceptors.response.use(
 	(response) => {
+		// 请求结束后将对应存储的取消函数删除
+		removeSource(response.config)
 		response.config.loading && store.dispatch('setLoading', false)
 
 		// 在这里你可以判断后台返回数据携带的请求码
@@ -49,7 +107,7 @@ service.interceptors.response.use(
 		error.config.loading && store.dispatch('setLoading', false)
 		let config = error.config
 
-    //如果config不存在或retry没设置，返回错误
+		//如果config不存在或retry没设置，返回错误
 		if (!config || !config.retry) return Promise.reject(error)
 
 		// 记录请求次数
@@ -64,7 +122,7 @@ service.interceptors.response.use(
 		config.__retryCount += 1
 
 		// 创建延时发起请求函数
-		var backoff = new Promise(function(resolve) {
+		let backoff = new Promise(function(resolve) {
 			setTimeout(function() {
 				resolve()
 			}, config.retryDelay || 1)
